11ce6340d1cdb1a53e47d08edb2f59ea6775b04e
Md. Wahiduzzaman <mdwahiduzzaman.wahid@bracits.com>
2023-07-27 17:52:29 +0600

    first commit

---
+++ src/anotherpackage/Class1.java
@@ -0,0 +1,9 @@
+package anotherpackage;
+import calculator.Calculator;
+
+public class Class1 {
+    public static void main(String args[]) {
+        Calculator obj = new Calculator();
+        System.out.println(obj.add(100, 200));
+    }
+}
